b44321ee2c6150d70f22a6463262b1642c473b47
konekos <newbiesad@gmail.com>
2021-03-19 18:40:17 +0800

    reactor Flux、Mono

---
+++ src/main/java/com/jasu/reactor/Generate.java
@@ -0,0 +1,17 @@
+package com.jasu.reactor;
+
+import reactor.core.publisher.Flux;
+
+public class Generate {
+    public static void main(String[] args) {
+
+        Flux<String> flux = Flux.generate(() -> 0, (state, sink) -> {
+            sink.next("3 x " + state + " = " + 3 * state);
+            if (state == 10) {
+                sink.complete();
+            }
+            return state + 1;
+        });
+        flux.doOnNext(s -> System.out.println(s)).subscribe();
+    }
+}
--- src/main/java/com/jasu/reactor/SampleSubscriber.java
+++ src/main/java/com/jasu/reactor/SampleSubscriber.java
@@ -2,16 +2,25 @@ package com.jasu.reactor;
 
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
 
 public class SampleSubscriber<T> extends BaseSubscriber<T> {
 
     @Override
     protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("subscribed");
         super.hookOnSubscribe(subscription);
     }
 
     @Override
     protected void hookOnNext(T value) {
-        super.hookOnNext(value);
+        System.out.println(value);
+        request(1);
+    }
+
+    public static void main(String[] args) {
+        SampleSubscriber<Integer> ss = new SampleSubscriber<Integer>();
+        Flux<Integer> ints = Flux.range(1, 4);
+        ints.subscribe(ss);
     }
 }
